40d4e5033da640d7668096220ec9c75a87cc008a
zukeras <lemuel@lemuel.pl>
2023-08-08 22:19:43 +0200

    committed

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "azure-terraform_01" {
+  name     = "terrform-resources"
+  location = "West Europe"
+}
