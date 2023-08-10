5e77842c553bde8de407799808d6e5ea6e7ec4d4
zukeras <lemuel@lemuel.pl>
2023-08-10 18:51:49 +0200

    changes

---
+++ Main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "azure-terraform_01" {
+  name     = "terraform-resources"
+  location = "West Europe"
+}
+resource "azurerm_virtual_network" "main_virtual_network" {
+  name = "main_network"
+  location = "westeurope"
+  resource_group_name = "terraform-resources"
+  address_space = [ "10.1.0.0/16" ]
+
+  subnet {
+    name = "dev_subnet"
+    address_prefix = "10.1.1.0/24"
+  }
+  subnet {
+    name = "test_subnet"
+    address_prefix = "10.1.2.0/24"
+  }
+}
+
