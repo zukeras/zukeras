ca1ad1457766d4308dd022d8ae132a67fbe42703
zukeras <lemuel@lemuel.pl>
2023-08-17 22:40:21 +0200

    changes

--- Main.tf
+++ Main.tf
@@ -16,17 +16,17 @@ resource "azurerm_resource_group" "azure-terraform_01" {
   location = "West Europe"
 }
 resource "azurerm_virtual_network" "main_virtual_network" {
-  name = "main_network"
-  location = "westeurope"
-  resource_group_name = "terraform-resources"
-  address_space = [ "10.1.0.0/16" ]
+  name                = "main_network"
+  location            = azurerm_resource_group.azure-terraform_01.location
+  resource_group_name = azurerm_resource_group.azure-terraform_01.name
+  address_space       = ["10.1.0.0/16"]
 
   subnet {
-    name = "dev_subnet"
+    name           = "dev_subnet"
     address_prefix = "10.1.1.0/24"
   }
   subnet {
-    name = "test_subnet"
+    name           = "test_subnet"
     address_prefix = "10.1.2.0/24"
   }
 }
