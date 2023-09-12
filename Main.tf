c9bf079c4e19f1945a0a6d212f7752fd8c972096
zukeras <lemuel@lemuel.pl>
2023-09-12 07:34:54 +0200

    changes

--- Main.tf
+++ Main.tf
@@ -31,3 +31,71 @@ resource "azurerm_virtual_network" "main_virtual_network" {
   }
 }
 
+resource "azurerm_public_ip" "linuxVM_ip" {
+  name                = "linuxVM_ip"
+  location            = azurerm_resource_group.azure-terraform_01.location
+  resource_group_name = azurerm_resource_group.azure-terraform_01.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "linuxVM_nic" {
+  name                = "linuxVM_nic"
+  location            = azurerm_resource_group.azure-terraform_01.location
+  resource_group_name = azurerm_resource_group.azure-terraform_01.name
+
+  ip_configuration {
+    name                          = "Internal"
+    subnet_id                     = azurerm_virtual_network.main_virtual_network.subnet.*.id[0]
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.linuxVM_ip.id
+  }
+
+}
+
+resource "azurerm_network_security_group" "nsg_shh" {
+  name                = "linuxVM_nsg"
+  location            = azurerm_resource_group.azure-terraform_01.location
+  resource_group_name = azurerm_resource_group.azure-terraform_01.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_network_interface_security_group_association" "nsg_association" {
+  network_interface_id = azurerm_network_interface.linuxVM_nic.id
+  network_security_group_id = azurerm_network_security_group.nsg_shh.id
+  
+}
+resource "azurerm_windows_virtual_machine" "linuxVM" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+
