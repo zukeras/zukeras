628d198171a45e79534733e30952d239fd5a952e
Pawel <92589562+zukeras@users.noreply.github.com>
2023-09-13 14:11:21 +0200

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -79,8 +79,12 @@ resource "azurerm_windows_virtual_machine" "linuxVM" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = "P@$$w0rd1234!"
+  admin_username      = "azureuser"
+  admin_ssh_key {
+    admin_username = "azureuser"
+    public_key = "./.ssh/id_rsa.pub"
+  }
+  
   network_interface_ids = [
     azurerm_network_interface.linuxVM_nic.id,
   ]
@@ -97,5 +101,3 @@ resource "azurerm_windows_virtual_machine" "linuxVM" {
     version   = "latest"
   }
 }
-
-
